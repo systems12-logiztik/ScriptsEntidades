e19668e46d5e6f20e9982ecc013175ee28675039
Cristhian Cuichan Logiztik Alliance <systems12@logiztikalliance.com>
2026-03-17 14:14:51 -0500

    fix Modificacion de scripts

--- 55188 ALTER TABLE/05-CreateIndexes_GuiasHouse_BilltoConsigneeId.sql
+++ 55188 ALTER TABLE/05-CreateIndexes_GuiasHouse_BilltoConsigneeId.sql
@@ -1,13 +1,18 @@
-SET ANSI_PADDING ON
+/*
+VERSION     MODIFIEDBY      	MODIFIEDDATE    HU      MODIFICATION
+1           Fernando Ordoñez     2026-02-27     57725   Optimized indexes for GuiasHouse
+*/
+
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'idx_GuiasHouse_BilltoConsigneeId')
+	BEGIN
+		CREATE NONCLUSTERED INDEX [idx_GuiasHouse_BilltoConsigneeId] ON [dbo].[GuiasHouse]
+		(
+			[BilltoConsigneeId] ASC,
+			[ConsigneeId] ASC
+		)
+		INCLUDE([id]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 50, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+	END
 GO
 
-/****** Object:  Index [idx_GuiasHouse_idCliente]    Script Date: 2/5/2026 5:44:42 PM ******/
-CREATE NONCLUSTERED INDEX [idx_GuiasHouse_BilltoConsigneeId] ON [dbo].[GuiasHouse]
-(
-	[BilltoConsigneeId] ASC,
-	[ConsigneeId] ASC
-)
-INCLUDE([id]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 50, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-GO
 
 
--- 55188 ALTER TABLE/06-CreateIndexes_GuiasHouseDetalles_shipToId.sql
+++ 55188 ALTER TABLE/06-CreateIndexes_GuiasHouseDetalles_shipToId.sql
@@ -1,15 +1,16 @@
+/*
+VERSION     MODIFIEDBY      	MODIFIEDDATE    HU      MODIFICATION
+1           Fernando Ordoñez     2026-02-27     57725   Optimized indexes for GuiasHouseDetalles
+*/
 
-
-SET ANSI_PADDING ON
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'idx_GuiasHouseDetalles_shipToId')
+	BEGIN
+		CREATE NONCLUSTERED INDEX [idx_GuiasHouseDetalles_shipToId] ON [dbo].[GuiasHouseDetalles]
+		(
+			[shipToId] ASC,
+			[estadoPieza] ASC
+		)
+		INCLUDE([id],[idGuiaHouse],[codigoBarra],[fechaCambio],[scanDespacho]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 80, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+	END
 GO
-
-/****** Object:  Index [idx_GuiasHouseDetalles_idClienteFinal]    Script Date: 2/5/2026 4:28:22 PM ******/
-CREATE NONCLUSTERED INDEX [idx_GuiasHouseDetalles_shipToId] ON [dbo].[GuiasHouseDetalles]
-(
-	[shipToId] ASC,
-	[estadoPieza] ASC
-)
-INCLUDE([id],[idGuiaHouse],[codigoBarra],[fechaCambio],[scanDespacho]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 80, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-GO
-
 
