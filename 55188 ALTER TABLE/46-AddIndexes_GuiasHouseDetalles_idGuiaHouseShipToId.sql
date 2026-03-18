9728532015aa9ac6b24ddab2914c8be00ba52f1a
Cristhian Cuichan Logiztik Alliance <systems12@logiztikalliance.com>
2026-03-18 09:41:30 -0500

    fix correccion scripts

--- 55188 ALTER TABLE/46-AddIndexes_GuiasHouseDetalles_idGuiaHouseShipToId.sql
+++ 55188 ALTER TABLE/46-AddIndexes_GuiasHouseDetalles_idGuiaHouseShipToId.sql
@@ -14,15 +14,6 @@ IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'idx_GuiasHouseDetalles_id
 	END
 GO
 
-IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'idx_GuiasHouseDetalles_shipToId')
-	BEGIN
-		CREATE NONCLUSTERED INDEX [idx_GuiasHouseDetalles_shipToId] ON [dbo].[GuiasHouseDetalles]
-		(
-			[shipToId] ASC,
-			[estadoPieza] ASC
-		)
-		INCLUDE([id],[idGuiaHouse],[codigoBarra],[fechaCambio],[scanDespacho]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 80, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-	END
-GO
+
 
 
