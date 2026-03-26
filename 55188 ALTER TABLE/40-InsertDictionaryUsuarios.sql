a2b5db9749ce7c658d3145f554e9acf12aa823c6
Cristhian Cuichan Logiztik Alliance <systems12@logiztikalliance.com>
2026-03-26 15:08:08 -0500

    fix modificacion

--- 55188 ALTER TABLE/40-InsertDictionaryUsuarios.sql
+++ 55188 ALTER TABLE/40-InsertDictionaryUsuarios.sql
@@ -1,9 +1,9 @@
 /*    
-VERSION		MODIFIEDBY		MODIFIEDDATE	HU				MODIFICATION
-1			Jorge Ortiz		2025-04-10		CT 46760		Initial Code - Add addextendedproperty for DetailDescription
+VERSION		MODIFIEDBY					MODIFIEDDATE	HU				MODIFICATION
+1			Cristhian Cuichan			2025-03-26		63742			Initial Code - Add addextendedproperty for EntityType field in Usuarios table
 */
-DECLARE @tabla VARCHAR(32) = 'ParametrosLista',
-		@columnDetailDescription VARCHAR(32) = 'DetailDescription',
+DECLARE @tabla VARCHAR(32) = 'Usuarios',
+		@columnEntityType VARCHAR(32) = 'EntityType',
 		@name VARCHAR(32) = 'MS_Description';
 
 IF EXISTS (
@@ -11,7 +11,7 @@ IF EXISTS (
 	FROM INFORMATION_SCHEMA.COLUMNS
 	WHERE TABLE_SCHEMA = 'dbo'
 	AND TABLE_NAME = @tabla
-	AND COLUMN_NAME = @columnDetailDescription
+	AND COLUMN_NAME = @columnEntityType
 )
 BEGIN
 	IF NOT EXISTS (
@@ -22,25 +22,25 @@ BEGIN
 		INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
 		WHERE s.name = 'dbo'
 		AND t.name = @tabla
-		AND c.name = @columnDetailDescription
+		AND c.name = @columnEntityType
 		AND ep.name = @name
 	)
 	BEGIN
 		-- Agregar extended property si no existe
 		EXEC sp_addextendedproperty 
 			@name = @name, 
-			@value = 'Json with descriptions in different languages. Ex: {"description":{"es-US":"Texto espaniol","en-US":"texto ingles"},"detail":{"es-US":"texto espaniol","en-US":"texto ingles"}}', 
+			@value = '1:BillTo,2:Consignee', 
 			@level0type = N'SCHEMA', @level0name = 'dbo', 
 			@level1type = N'TABLE',  @level1name = @tabla, 
-			@level2type = N'COLUMN', @level2name = @columnDetailDescription;
-		PRINT 'DICTIONARY INSERTED INTO COLUMN '+ @columnDetailDescription;
+			@level2type = N'COLUMN', @level2name = @columnEntityType;
+		PRINT 'DICTIONARY INSERTED INTO COLUMN '+ @columnEntityType;
 	END
 	ELSE	
 	BEGIN
-		PRINT 'EXTENDED PROPERTY ALREADY EXISTS FOR COLUMN '+ @columnDetailDescription + ' - NO ACTION TAKEN';
+		PRINT 'EXTENDED PROPERTY ALREADY EXISTS FOR COLUMN '+ @columnEntityType + ' - NO ACTION TAKEN';
 	END
 END
 ELSE
 BEGIN
-	PRINT 'TABLE ' + @tabla + ' OR COLUMN ' + @columnDetailDescription + ' DOES NOT EXIST - NO ACTION TAKEN';
+	PRINT 'TABLE ' + @tabla + ' OR COLUMN ' + @columnEntityType + ' DOES NOT EXIST - NO ACTION TAKEN';
 END
