834f3bebebc0179c10535d415429ad964ddcd691
Cristhian Cuichan Logiztik Alliance <systems12@logiztikalliance.com>
2026-04-06 08:33:51 -0500

    add cambios de sp 57730 y 57733

--- 57723 STORE PROCEDURE/09-AC_pro_GetClientsEntities.sql
+++ 57723 STORE PROCEDURE/09-AC_pro_GetClientsEntities.sql
@@ -56,6 +56,7 @@ BEGIN
             FROM Clientes C WITH (NOLOCK)
             LEFT JOIN dbo.f_SearchEntities('', 'Consignee') FSE ON C.id = FSE.IdCliente
             WHERE C.id = @EntityId
+			AND C.[status] = 'ACTIVO'
             RETURN
         END
         -- =============================================================================================
@@ -67,6 +68,7 @@ BEGIN
             FROM GrupoClientes GC WITH (NOLOCK)
             LEFT JOIN dbo.f_SearchEntities('', 'Consignee') FSE ON GC.IdCliente = FSE.IdCliente
             WHERE GC.IdGrupoCliente = @EntityId
+			AND GC.[status] = 'ACTIVO'
             RETURN
         END
 
