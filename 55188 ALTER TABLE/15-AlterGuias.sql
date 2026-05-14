09a67c654d48aeeeb7d90bb80d99720ac5d8f6e1
Cristhian Cuichan Logiztik Alliance <systems12@logiztikalliance.com>
2026-05-14 11:23:46 -0500

    fix modificacion guias

--- 55188 ALTER TABLE/15-AlterGuias.sql
+++ 55188 ALTER TABLE/15-AlterGuias.sql
@@ -1,15 +1,14 @@
 /*
 VERSION     MODIFIEDBY					MODIFIEDDATE    HU          MODIFICATION
-1           Cristhian Cuichan           2026-02-13      55188       Initial Code - Add Columns ConsigneeId and BillToConsigneeId into Guias 
+1           Cristhian Cuichan           2026-02-13      55188       Initial Code - Add Columns BillToConsigneeId into Guias 
 */
 
 IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID('Guias') AND name = 'ConsigneeId')
 BEGIN
-    ALTER TABLE Guias ADD ConsigneeId VARCHAR(16) NULL,
-						BillToConsigneeId VARCHAR(16) NULL;
-    PRINT 'Columna ConsigneeId y BillToConsigneeId agregada a Guias';
+    ALTER TABLE Guias ADD BillToConsigneeId VARCHAR(16) NULL;
+    PRINT 'Columna BillToConsigneeId agregada a Guias';
 END
 ELSE
 BEGIN
-    PRINT 'Columna ConsigneeId y BillToConsigneeId ya existe en Guias';
+    PRINT 'Columna BillToConsigneeId ya existe en Guias';
 END
