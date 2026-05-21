714a685be6945370c4287f6fe94e55f733e03d62
Cristhian Cuichan Logiztik Alliance <systems12@logiztikalliance.com>
2026-05-21 11:22:28 -0500

    fix

--- 55188 ALTER TABLE/45-InsertCatalogosUserRoles.sql
+++ 55188 ALTER TABLE/45-InsertCatalogosUserRoles.sql
@@ -3,67 +3,22 @@ VERSION		MODIFIEDBY				MODIFIEDDATE	HU					MODIFICATION
 1			Mateo Velasco			2025-06-25		55188				Initial Code - Creation of Consignees Rol
 2			Jorge Ortiz			    2025-07-24		LAG-CT-013 53071	Initial Code - Add new Catalog
 3           Ian Carlos Ortega       2025-12-15      LAG-CT-019 55188    Add new role called 'CLIENTE' to UserRoles catalog
-4           Jair Gomez              2026-04-14      LAG-CT-019 55188    Add suffix _TYPE to UserRoles catalog 
+4           Jair Gomez              2026-04-14      LAG-CT-019 55188    Add suffix _TYPE to UserRoles catalog
+5           Jair Gomez              2026-05-10      LAG-CT-033 65319    Eliminate BILLTO AND CONSIGNEE role
 */
 IF NOT EXISTS (
       SELECT 1
       FROM [dbo].[Catalogos]
       WHERE [codigo] = 'UserRoles' 
       AND [identificador] IN (
-            'AGENCIA_CARGA_TYPE',
             'BROKER_TYPE',
             'EMPLEADO_TYPE',
             'EXPORTADOR_TYPE',
-            'BILLTO_TYPE',
-            'CONSIGNEE_TYPE',
             'CLIENTE_TYPE'
       )
 )
 BEGIN
     DECLARE @IdNew INT;
-    SELECT @IdNew = MAX([IdNew]) + 1
-    FROM [dbo].[Catalogos];
-
-    INSERT INTO [dbo].[Catalogos]
-        (
-        [idEmpresa]
-        ,[nombre]
-        ,[nombreIngles]
-        ,[codigo]
-        ,[tipo]
-        ,[descripcion]
-        ,[status]
-        ,[nota]
-        ,[fechaCambio]
-        ,[identificador]
-        ,[clase]
-        ,[codigoRelacion]
-        ,[orden]
-        ,[oculto]
-        ,[idCatalgoPadre]
-        ,[idRegistroVinculado]
-        ,[IdNew]
-        )
-    VALUES
-        (
-        NULL
-        ,'AGENCIA DE CARGA'
-        ,'CARGO AGENCY'
-        ,'UserRoles'
-        ,'ENUMERACION'
-        ,'User role type for listing'
-        ,'ACTIVO'
-        ,NULL
-        ,GETDATE()
-        ,'AGENCIA_CARGA_TYPE'
-        ,NULL
-        ,NULL
-        ,1
-        ,0
-        ,NULL
-        ,NULL
-        ,@IdNew
-        );
 
     SELECT @IdNew = MAX([IdNew]) + 1
     FROM [dbo].[Catalogos];
@@ -101,7 +56,7 @@ BEGIN
         ,'BROKER_TYPE'
         ,NULL
         ,NULL
-        ,2
+        ,1
         ,0
         ,NULL
         ,NULL
@@ -144,7 +99,7 @@ BEGIN
         ,'EMPLEADO_TYPE'
         ,NULL
         ,NULL
-        ,4
+        ,2
         ,0
         ,NULL
         ,NULL
@@ -187,98 +142,11 @@ BEGIN
         ,'EXPORTADOR_TYPE'
         ,NULL
         ,NULL
-        ,5
-        ,0
-        ,NULL
-        ,NULL
-        ,@IdNew
-        );
-
-    SELECT @IdNew = MAX([IdNew]) + 1
-    FROM [dbo].[Catalogos];
-    INSERT INTO [dbo].[Catalogos]
-        (
-        [idEmpresa]
-        ,[nombre]
-        ,[nombreIngles]
-        ,[codigo]
-        ,[tipo]
-        ,[descripcion]
-        ,[status]
-        ,[nota]
-        ,[fechaCambio]
-        ,[identificador]
-        ,[clase]
-        ,[codigoRelacion]
-        ,[orden]
-        ,[oculto]
-        ,[idCatalgoPadre]
-        ,[idRegistroVinculado]
-        ,[IdNew]
-        )
-    VALUES
-        (
-        NULL
-        ,'BILL TO'
-        ,'BILL TO'
-        ,'UserRoles'
-        ,'ENUMERACION'
-        ,'User role type for listing'
-        ,'ACTIVO'
-        ,NULL
-        ,GETDATE()
-        ,'BILLTO_TYPE'
-        ,NULL
-        ,NULL
-        ,7
-        ,0
-        ,NULL
-        ,NULL
-        ,@IdNew
-        );
-
-    SELECT @IdNew = MAX([IdNew]) + 1
-    FROM [dbo].[Catalogos];
-
-    INSERT INTO [dbo].[Catalogos]
-        (
-        [idEmpresa]
-        ,[nombre]
-        ,[nombreIngles]
-        ,[codigo]
-        ,[tipo]
-        ,[descripcion]
-        ,[status]
-        ,[nota]
-        ,[fechaCambio]
-        ,[identificador]
-        ,[clase]
-        ,[codigoRelacion]
-        ,[orden]
-        ,[oculto]
-        ,[idCatalgoPadre]
-        ,[idRegistroVinculado]
-        ,[IdNew]
-        )
-    VALUES
-        (
-        NULL
-        ,'CONSIGNATARIOS'
-        ,'CONSIGNEES'
-        ,'UserRoles'
-        ,'ENUMERACION'
-        ,'User role type for listing'
-        ,'ACTIVO'
-        ,NULL
-        ,GETDATE()
-        ,'CONSIGNEE_TYPE'
-        ,NULL
-        ,NULL
         ,3
         ,0
         ,NULL
-        ,NULL,
-        @IdNew
+        ,NULL
+        ,@IdNew
         );
 
     SELECT @IdNew = MAX([IdNew]) + 1
@@ -318,15 +186,10 @@ BEGIN
         ,'CLIENTE_TYPE'
         ,NULL
         ,NULL
-        ,8
+        ,4
         ,0
         ,NULL
         ,NULL,
         @IdNew
         );
 END;
-
-
-
-
-
